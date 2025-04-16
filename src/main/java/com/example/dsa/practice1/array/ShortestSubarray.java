b85f1bf8670acad56a19bc7264b261f2232eecc8
kcsahu <kailashsahu.official@gmail.com>
2025-04-16 21:01:28 +0530

    DS Algo

---
+++ src/main/java/com/example/dsa/practice1/array/ShortestSubarray.java
@@ -0,0 +1,39 @@
+package com.example.dsa.practice1.array;
+
+import java.util.Deque;
+import java.util.LinkedList;
+
+// Two pointers and Prefix-sum
+// Array with integers
+public class ShortestSubarray {
+
+    public static void main(String[] args) {
+
+        int[] nums = {17, 85, 93, -45, -21};
+        System.out.println(shortestSubarray(nums, 150));
+    }
+
+
+    private static int shortestSubarray(int[] nums, int k){
+        int len = nums.length;
+        long[] prefixSum = new long[len + 1];
+        for(int i=0; i<len; i++){
+            prefixSum[i + 1] = prefixSum[i] + nums[i];
+        }
+        Deque<Integer> dq = new LinkedList<>();
+        int minLen = Integer.MAX_VALUE;
+        for(int i=0; i< prefixSum.length; i++){
+            while (dq.size() > 0 && prefixSum[i] - prefixSum[dq.peekFirst()] >= k){
+                minLen = Math.min(minLen, i - dq.pollFirst());
+            }
+
+            while(dq.size()>0 && prefixSum[i] <= prefixSum[dq.peekLast()]){
+                dq.pollLast();
+            }
+            dq.offerLast(i);
+        }
+        return minLen != Integer.MAX_VALUE ? minLen : -1;
+    }
+
+
+}
