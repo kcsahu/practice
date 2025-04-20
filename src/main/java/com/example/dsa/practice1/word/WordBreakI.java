299e636a42aa96f57a5e0c340830a6b9883c4828
kcsahu <kailashsahu.official@gmail.com>
2025-04-20 11:07:03 +0530

    DS Algo

--- src/main/java/com/example/dsa/practice1/word/WordBreakI.java
+++ src/main/java/com/example/dsa/practice1/word/WordBreakI.java
@@ -1,19 +1,37 @@
 package com.example.dsa.practice1.word;
 
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Stream;
+
 /**
- * /**
- *  * Given a string s and a dictionary of strings wordDict, return true if s can be segmented into a space-separated
- *  * sequence of one or more dictionary words.
- *  * <p>
- *  * Note that the same word in the dictionary may be reused multiple times in the segmentation.
- *  * <p>
- *  * <p>
- *  * <p>
- *  * Example 1:
- *  * <p>
- *  * Input: s = "leetcode", wordDict = ["leet","code"]
- *  * Output: true
- *  * Explanation: Return true because "leetcode" can be segmented as "leet code".
- *  */
+ * Given a string s and a dictionary of strings wordDict, return true if s can be segmented into a
+ * space-separated sequence of one or more dictionary words.
+ * <p>
+ * Note that the same word in the dictionary may be reused multiple times in the segmentation.
+ * Input: s = "leetcode", wordDict = ["leet","code"]
+ * Output: true
+ * Explanation: Return true because "leetcode" can be segmented as "leet code".
+ */
 public class WordBreakI {
+    public static void main(String[] args) {
+        String s = "leetcode";
+        boolean result = wordBreak(s, Stream.of("leet","code").toList());
+        System.out.println(result);
+    }
+    public static boolean wordBreak(String s, List<String> wordDict) {
+        Set<String> dict = new HashSet<>(wordDict);
+        boolean[] dp = new boolean[s.length() + 1];
+        dp[0] = true;
+        for (int i = 1; i < dp.length; i++) {
+            for (int j = 0; j < i; j++) {
+                if(dp[j] && dict.contains(s.substring(j, i))){
+                    dp[i] = true;
+                    break;
+                }
+            }
+        }
+        return dp[s.length()];
+    }
 }
