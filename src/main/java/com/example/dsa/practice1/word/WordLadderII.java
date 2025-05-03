1b87ae4dfc56198b3ad2c1fd8a952c868eeec1f8
kcsahu <kailashsahu.official@gmail.com>
2025-05-03 20:09:40 +0530

    DS Algo

--- src/main/java/com/example/dsa/practice1/word/WordLadderII.java
+++ src/main/java/com/example/dsa/practice1/word/WordLadderII.java
@@ -66,35 +66,79 @@ public class WordLadderII {
                 "gte","sox","lei","mig","fig","lon","use","ban","flo","nov","jut","bag","mir","sty","lap","two","ins",
                 "con","ant","net","tux","ode","stu","mug","cad","nap","gun","fop","tot","sow","sal","sic","ted","wot",
                 "del","imp","cob","way","ann","tan","mci","job","wet","ism","err","him","all","pad","hah","hie","aim").toList()));
+        System.out.println(result);
+
+//        result = findLadders("aaaaa", "ggggg", Stream.of("aaaaa","caaaa","cbaaa","daaaa",
+//                "dbaaa","eaaaa","ebaaa","faaaa","fbaaa","gaaaa","gbaaa","haaaa","hbaaa","iaaaa","ibaaa","jaaaa","jbaaa",
+//                "kaaaa","kbaaa","laaaa","lbaaa","maaaa","mbaaa","naaaa","nbaaa","oaaaa","obaaa","paaaa","pbaaa","bbaaa",
+//                "bbcaa","bbcba","bbdaa","bbdba","bbeaa","bbeba","bbfaa","bbfba","bbgaa","bbgba","bbhaa","bbhba","bbiaa",
+//                "bbiba","bbjaa","bbjba","bbkaa","bbkba","bblaa","bblba","bbmaa","bbmba","bbnaa","bbnba","bboaa","bboba",
+//                "bbpaa","bbpba","bbbba","abbba","acbba","dbbba","dcbba","ebbba","ecbba","fbbba","fcbba","gbbba","gcbba",
+//                "hbbba","hcbba","ibbba","icbba","jbbba","jcbba","kbbba","kcbba","lbbba","lcbba","mbbba","mcbba","nbbba",
+//                "ncbba","obbba","ocbba","pbbba","pcbba","ccbba","ccaba","ccaca","ccdba","ccdca","cceba","cceca","ccfba",
+//                "ccfca","ccgba","ccgca","cchba","cchca","cciba","ccica","ccjba","ccjca","cckba","cckca","cclba","cclca",
+//                "ccmba","ccmca","ccnba","ccnca","ccoba","ccoca","ccpba","ccpca","cccca","accca","adcca","bccca","bdcca",
+//                "eccca","edcca","fccca","fdcca","gccca","gdcca","hccca","hdcca","iccca","idcca","jccca","jdcca","kccca",
+//                "kdcca","lccca","ldcca","mccca","mdcca","nccca","ndcca","occca","odcca","pccca","pdcca","ddcca","ddaca",
+//                "ddbca","ddbda","ddeca","ddeda","ddfca","ddfda","ddgca","ddgda","ddhca","ddhda","ddica","ddida","ddjca",
+//                "ddjda","ddkca","ddkda","ddlca","ddlda","ddmca","ddmda","ddnca","ddnda","ddoca","ddoda","ddpca","ddpda",
+//                "dddda","addda","aedda","bddda","bedda","cddda","cedda","fddda","fedda","gddda","gedda","hddda","hedda",
+//                "iddda","iedda","jddda","jedda","kddda","kedda","lddda","ledda","mddda","medda","nddda","nedda","oddda",
+//                "oedda","pddda","pedda","eedda","eeada","eeaea","eebda","eebea","eecda","eecea","eefda","eefea","eegda",
+//                "eegea","eehda","eehea","eeida","eeiea","eejda","eejea","eekda","eekea","eelda","eelea","eemda","eemea",
+//                "eenda","eenea","eeoda","eeoea","eepda","eepea","eeeea","ggggg","agggg","ahggg","bgggg","bhggg","cgggg",
+//                "chggg","dgggg","dhggg","egggg","ehggg","fgggg","fhggg","igggg","ihggg","jgggg","jhggg","kgggg","khggg",
+//                "lgggg","lhggg","mgggg","mhggg","ngggg","nhggg","ogggg","ohggg","pgggg","phggg","hhggg","hhagg","hhahg",
+//                "hhbgg","hhbhg","hhcgg","hhchg","hhdgg","hhdhg","hhegg","hhehg","hhfgg","hhfhg","hhigg","hhihg","hhjgg",
+//                "hhjhg","hhkgg","hhkhg","hhlgg","hhlhg","hhmgg","hhmhg","hhngg","hhnhg","hhogg","hhohg","hhpgg","hhphg",
+//                "hhhhg","ahhhg","aihhg","bhhhg","bihhg","chhhg","cihhg","dhhhg","dihhg","ehhhg","eihhg","fhhhg","fihhg",
+//                "ghhhg","gihhg","jhhhg","jihhg","khhhg","kihhg","lhhhg","lihhg","mhhhg","mihhg","nhhhg","nihhg","ohhhg",
+//                "oihhg","phhhg","pihhg","iihhg","iiahg","iiaig","iibhg","iibig","iichg","iicig","iidhg","iidig","iiehg",
+//                "iieig","iifhg","iifig","iighg","iigig","iijhg","iijig","iikhg","iikig","iilhg","iilig","iimhg","iimig",
+//                "iinhg","iinig","iiohg","iioig","iiphg","iipig","iiiig","aiiig","ajiig","biiig","bjiig","ciiig","cjiig",
+//                "diiig","djiig","eiiig","ejiig","fiiig","fjiig","giiig","gjiig","hiiig","hjiig","kiiig","kjiig","liiig",
+//                "ljiig","miiig","mjiig","niiig","njiig","oiiig","ojiig","piiig","pjiig","jjiig","jjaig","jjajg","jjbig",
+//                "jjbjg","jjcig","jjcjg","jjdig","jjdjg","jjeig","jjejg","jjfig","jjfjg","jjgig","jjgjg","jjhig","jjhjg",
+//                "jjkig","jjkjg","jjlig","jjljg","jjmig","jjmjg","jjnig","jjnjg","jjoig","jjojg","jjpig","jjpjg","jjjjg",
+//                "ajjjg","akjjg","bjjjg","bkjjg","cjjjg","ckjjg","djjjg","dkjjg","ejjjg","ekjjg","fjjjg","fkjjg","gjjjg",
+//                "gkjjg","hjjjg","hkjjg","ijjjg","ikjjg","ljjjg","lkjjg","mjjjg","mkjjg","njjjg","nkjjg","ojjjg","okjjg",
+//                "pjjjg","pkjjg","kkjjg","kkajg","kkakg","kkbjg","kkbkg","kkcjg","kkckg","kkdjg","kkdkg","kkejg","kkekg",
+//                "kkfjg","kkfkg","kkgjg","kkgkg","kkhjg","kkhkg","kkijg","kkikg","kkljg","kklkg","kkmjg","kkmkg","kknjg",
+//                "kknkg","kkojg","kkokg","kkpjg","kkpkg","kkkkg","ggggx","gggxx","ggxxx","gxxxx","xxxxx","xxxxy","xxxyy",
+//                "xxyyy","xyyyy","yyyyy","yyyyw","yyyww","yywww","ywwww","wwwww","wwvww","wvvww","vvvww","vvvwz","avvwz",
+//                "aavwz","aaawz","aaaaz").toList());
     }
 
     public static List<List<String>> findLadders(String beginWord, String endWord, List<String> wordList) {
         int len = wordList.size();
+        Set<String> wordDict = new LinkedHashSet<>(wordList);
         Queue<State> queue = new LinkedList<>();
         List<String> ladders = new LinkedList<>();
         ladders.add(beginWord);
-        queue.offer(new State(beginWord, ladders, new HashSet<>()));
+        queue.offer(new State(beginWord, ladders));
         List<List<String>> result = new LinkedList<>();
         int minSize = Integer.MAX_VALUE;
         while (queue.size() > 0) {
-            State curState = queue.poll();
-            List<String> curLadders = curState.ladders;
-            Set<Integer> curVisited = curState.hasVisited;
-            if (endWord.equals(curState.word) && curState.ladders.size() <= minSize) {
-                result.add(curState.ladders);
-                minSize = curState.ladders.size();
-                continue;
-            }
-            for (int i = 0; i < len; i++) {
-                String nextWord = wordList.get(i);
-                if (!curVisited.contains(i) && isAdjacent(curState.word, nextWord)) {
-                    List<String> nextLadders = new LinkedList<>(curLadders);
-                    nextLadders.add(nextWord);
-                    Set<Integer> nextVisited = new HashSet<>(curVisited);
-                    nextVisited.add(i);
-                    queue.offer(new State(nextWord, nextLadders, nextVisited));
+            int qsize = queue.size();
+            Set<String> visitedWords = new HashSet<>();
+            for(int i=0; i<qsize; i++) {
+                State curState = queue.poll();
+                List<String> curLadders = curState.ladders;
+                if (endWord.equals(curState.word) && curState.ladders.size() <= minSize) {
+                    result.add(curState.ladders);
+                    minSize = curState.ladders.size();
+                    continue;
+                }
+                for (String nextWord : wordDict) {
+                    if (isAdjacent(curState.word, nextWord)) {
+                        List<String> nextLadders = new LinkedList<>(curLadders);
+                        nextLadders.add(nextWord);
+                        queue.offer(new State(nextWord, nextLadders));
+                        visitedWords.add(nextWord);
+                    }
                 }
             }
+            wordDict.removeAll(visitedWords);
         }
         return result;
     }
@@ -102,12 +146,10 @@ public class WordLadderII {
     private static class State {
         String word;
         List<String> ladders;
-        Set<Integer> hasVisited;
 
-        State(String word, List<String> ladders, Set<Integer> hasVisited) {
+        State(String word, List<String> ladders) {
             this.word = word;
             this.ladders = ladders;
-            this.hasVisited = hasVisited;
         }
     }
 
