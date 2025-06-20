1a30d949b81d5e4acffa3af771c2c034038f99e3
kcsahu <kailashsahu.official@gmail.com>
2025-06-20 16:38:24 +0530

    DS Algo

--- src/main/java/com/example/dsa/practice1/linkedlist/RemoveNthNode.java
+++ src/main/java/com/example/dsa/practice1/linkedlist/RemoveNthNode.java
@@ -9,7 +9,7 @@ public class RemoveNthNode {
         ListNode head = LinkedListUtil.createLinkedList(new int[]{1,2,3,4,5,6,7,8,9});
         LinkedListUtil.print(removeNthNode(head, 4));
         head = LinkedListUtil.createLinkedList(new int[]{1,2,3,4,5,6,7,8,9});
-        LinkedListUtil.print(removeNthNode(head, 3));
+        LinkedListUtil.print(removeNthNode1(head, 12));
     }
     /**
      * [1,2,3,4,5,6,7,8,9], 4 => [1,2,3,4,5,7,8,9]
@@ -17,7 +17,7 @@ public class RemoveNthNode {
      * @param n
      * @return
      */
-    public static ListNode removeNthNode(ListNode head, int n){
+    public static ListNode removeNthNode1(ListNode head, int n){
         ListNode node1 = head;
         while(n > 0){
             node1 = node1.next;
@@ -30,6 +30,24 @@ public class RemoveNthNode {
         while(node1.next != null){
             node2 = node2.next;
             node1 = node1.next;
+        }
+        node2.next = node2.next.next;
+        return head;
+    }
+
+    public static ListNode removeNthNode(ListNode head, int n){
+        ListNode node1 = head;
+        ListNode node2 = head;
+        while(node1.next != null){
+            node1 = node1.next;
+            if(n > 0){
+                n--;
+            } else{
+                node2 = node2.next;
+            }
+        }
+        if(node1.next == null && n > 0){
+            return head;
         }
         node2.next = node2.next.next;
         return head;
