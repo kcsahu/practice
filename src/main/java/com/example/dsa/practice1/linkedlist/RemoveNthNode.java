0706b963e51f71d267bcbb167a92953ca21a882f
kcsahu <kailashsahu.official@gmail.com>
2025-10-10 06:09:33 +0530

    DS Algo

--- src/main/java/com/example/dsa/practice1/linkedlist/RemoveNthNode.java
+++ src/main/java/com/example/dsa/practice1/linkedlist/RemoveNthNode.java
@@ -9,7 +9,10 @@ public class RemoveNthNode {
         ListNode head = LinkedListUtil.createLinkedList(new int[]{1,2,3,4,5,6,7,8,9});
         LinkedListUtil.print(removeNthNode(head, 4));
         head = LinkedListUtil.createLinkedList(new int[]{1,2,3,4,5,6,7,8,9});
-        LinkedListUtil.print(removeNthNode1(head, 12));
+        LinkedListUtil.print(removeNthNode(head, 12));
+        head = LinkedListUtil.createLinkedList(new int[]{1});
+        head = removeNthNode(head, 1);
+        LinkedListUtil.print(head);
     }
     /**
      * [1,2,3,4,5,6,7,8,9], 4 => [1,2,3,4,5,7,8,9]
@@ -17,27 +20,11 @@ public class RemoveNthNode {
      * @param n
      * @return
      */
-    public static ListNode removeNthNode1(ListNode head, int n){
-        ListNode node1 = head;
-        while(n > 0){
-            node1 = node1.next;
-            n -= 1;
-            if(node1 == null){
-                return head;
-            }
-        }
-        ListNode node2 = head;
-        while(node1.next != null){
-            node2 = node2.next;
-            node1 = node1.next;
-        }
-        node2.next = node2.next.next;
-        return head;
-    }
-
     public static ListNode removeNthNode(ListNode head, int n){
-        ListNode node1 = head;
-        ListNode node2 = head;
+        ListNode result = new ListNode(-1);
+        result.next = head;
+        ListNode node1 = result;
+        ListNode node2 = result;
         while(node1.next != null){
             node1 = node1.next;
             if(n > 0){
@@ -47,9 +34,9 @@ public class RemoveNthNode {
             }
         }
         if(node1.next == null && n > 0){
-            return head;
+            return result.next;
         }
         node2.next = node2.next.next;
-        return head;
+        return result.next;
     }
 }
